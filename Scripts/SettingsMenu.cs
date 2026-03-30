9ad6f99845a52a5d91015cf60ab0bac52eb2a654
AapoAkonniemi1 <aapo.akonniemi@tuni.fi>
2026-03-30 16:28:51 +0300

    settings menu fixed, finnish and english flags added

--- Scripts/SettingsMenu.cs
+++ Scripts/SettingsMenu.cs
@@ -3,18 +3,33 @@ using System;
 
 public partial class SettingsMenu : CanvasLayer
 {
-    [Export] private Button _CloseButton; // liitä Inspectorissa CloseButton tähän
+    [Export] private TextureButton _CloseButton; // liitä Inspectorissa CloseButton tähän
+    [Export] private TextureButton _FinnishButton;
+    [Export] private TextureButton _EnglishButton;
+
 
     public override void _Ready()
     {
         // Liitetään CloseButton piilottamaan koko SettingsMenu
         _CloseButton.Pressed += OnClosePressed;
 
+        _FinnishButton.Pressed += OnFinnishPressed;
+
+        _EnglishButton.Pressed += OnEnglishPressed;
+
         // Piilotetaan SettingsMenu aluksi
         Visible = false;
     }
 
     private void OnClosePressed()
+    {
+        Visible = false;
+    }
+    private void OnFinnishPressed()
+    {
+        Visible = false;
+    }
+    private void OnEnglishPressed()
     {
         Visible = false;
     }
