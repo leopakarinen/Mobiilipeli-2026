1e9463770d06caf985695d4a6371e19a831f4376
AapoAkonniemi1 <aapo.akonniemi@tuni.fi>
2026-03-31 14:58:08 +0300

    game over scene added sexy things.

--- Scripts/GameOver.cs
+++ Scripts/GameOver.cs
@@ -5,17 +5,25 @@ public partial class GameOver : Control
 {
     [Export] private Button _PlayAgainButton;
     [Export] private Button _QuitButton;
+    [Export] private Button _MainmenuButton;
 
     public override void _Ready()
     {
         _PlayAgainButton.Pressed += OnPlayAgainPressed;
         _QuitButton.Pressed += OnQuitPressed;
+        _MainmenuButton.Pressed += OnMainmenuButtonPressed;
     }
 
     private void OnPlayAgainPressed()
     {
         GetTree().ChangeSceneToFile("res://Scenes/Main.tscn");
         // restarts game
+    }
+
+private void OnMainmenuButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
+        // goes to main menu
     }
 
     private void OnQuitPressed()
